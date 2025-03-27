275cc86aee29e71d406b93285bee2f0ee9c630db
Nika Maciel <49723423+nikamaciel@users.noreply.github.com>
2025-03-27 01:14:48 -0300

    Update mineracao.py

--- mineracao.py
+++ mineracao.py
@@ -1,14 +1,8 @@
-import os
-from dotenv import load_dotenv  # Adicionando dotenv para carregar as variáveis
-
 import requests
 
-# Carregar variáveis do arquivo .env
-load_dotenv()  # Isso vai carregar as variáveis para o ambiente
-
-# Agora você pode acessar a variável de ambiente
-SHOPIFY_ACCESS_TOKEN = os.getenv("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")
-SHOPIFY_STORE_URL = "https://enigmafemme.com/"
+# Token de Acesso da API Admin
+SHOPIFY_ACCESS_TOKEN = "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"
+SHOPIFY_STORE_URL = "https://enigmafemme.com"  # URL da sua loja
 SHOPIFY_API_VERSION = "2023-01"
 
 # URL da API para criar produtos
@@ -17,7 +11,7 @@ url = f"{SHOPIFY_STORE_URL}/admin/api/{SHOPIFY_API_VERSION}/products.json"
 # Cabeçalhos com autenticação correta
 headers = {
     "Content-Type": "application/json",
-    "X-Shopify-Access-Token": SHOPIFY_ACCESS_TOKEN  # Usando o token da variável de ambiente
+    "X-Shopify-Access-Token": SHOPIFY_ACCESS_TOKEN  # Usando o Token de Acesso da API Admin
 }
 
 # Dados de um produto de teste
@@ -35,4 +29,3 @@ response = requests.post(url, json=data, headers=headers)
 
 # Verificando se funcionou
 print(response.status_code, response.json())
-
