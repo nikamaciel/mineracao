0dab3976b8cfaf13a0e0ccf50f866e4fa46c3fc1
Nika Maciel <49723423+nikamaciel@users.noreply.github.com>
2025-03-27 22:13:35 -0300

    Update mineracao.py

--- mineracao.py
+++ mineracao.py
@@ -37,11 +37,12 @@ for produto in produtos_extraidos:
     headers = {
         "Content-Type": "application/json",
         "X-Shopify-Access-Token": SHOPIFY_ACCESS_TOKEN
+        
     }
     
     print(f"📤 Enviando produto '{produto['title']}' para o Shopify...")
 
-    response = requests.post(SHOPIFY_API_URL, headers=headers, json=payload)
+    response = requests.post(url, auth=(fe651dbae88b7513021c8461fabff8c4Y, 617d6bfbf8622c9c731a8e6f7177724d), json=data)
 
     # 📝 Verificando a resposta do Shopify
     print("📝 Resposta do Shopify:", response.status_code, response.text)
